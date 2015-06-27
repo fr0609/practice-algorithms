02352d311a69cbf166271e68b09c286578afc02d
frarlon_rodriguez <fr0609@gmail.com>
2015-06-27 11:04:02 -0600

    15

---
+++ 15.js
@@ -0,0 +1,17 @@
+(function euler015(){
+	       var  gridSize = 2;
+           var grid = [];
+
+            for (var i = 0; i < gridSize; i++) {
+                grid[i, gridSize] = 1;
+                grid[gridSize,i] = 1;
+            }
+
+           for (var i = gridSize - 1; i >= 0; i--) {
+                for (var j = gridSize - 1; j >= 0; j--) {
+                    grid[i, j] = grid[i+1, j] + grid[i, j+1];
+            }
+        }
+
+            console.log(grid[2,2]);
+} ) ();
--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,17 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
  
 gulp.task('lint', function() {
-  return gulp.src('./*.js')
+  return gulp.src('./15.js')
     .pipe(jshint())
     .pipe(jshint.reporter('gulp-jshint-file-reporter', {
-      filename: __dirname + '/jshint-output.log'
+      filename :'./jshint-output.log'
     }));
 });
+
+ gulp.task('watch', function() {
+
+
+      gulp.watch('./15.js', function() {
+        gulp.run('lint');
+ });
+});      

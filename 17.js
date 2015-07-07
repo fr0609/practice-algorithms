5fbf179bb38b0c0dc00c2e0ebd345a45996a3a2f
frarlon_rodriguez <fr0609@gmail.com>
2015-07-06 21:56:32 -0600

    17 in progress

---
+++ 17.js
@@ -0,0 +1,39 @@
+(function euler0017(){
+var num = [
+	'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'eighteen',
+    'nineteen',
+    'twenty',
+    'twenty-one',
+    'twenty-two',
+    'twenty-three',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+    'one hundred and one',
+    'two hundred',
+    'three hundred',
+    'one thousand'
+];
+
+
+})();
